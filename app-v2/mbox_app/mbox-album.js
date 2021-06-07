390f86f91c91a91956634c1fd62c1e3870cd84d3
Christoph <email@ckloth.de>
2021-06-07 22:35:59 +0200

    app: list album covers incl. details bugfix and optimize

--- app-v2/mbox_app/mbox-album.js
+++ app-v2/mbox_app/mbox-album.js
@@ -55,6 +55,15 @@ function mboxListCount() {
 	return mbox_list_count;
 	}
 
+function mboxListCount_New() {
+	setTextById("mbox_temp","<div class=\"album_cover\" id=\"album_test_size\">test</div>" );
+	var row_width       = document.getElementById("frame3").offsetWidth - 10; 		//width - 2x padding
+	var album_width     = document.getElementById("album_test_size").offsetWidth + 6;	//width + 2x margin
+	var max_albums      = Math.round(row_width/album_width);
+	setTextById("mbox_temp","");
+	return max_albums;
+	}
+
 
 // List albums
 //--------------------------------------
@@ -86,9 +95,9 @@ function mboxAlbumAll(data) {
 	var album_active     = "";
 	var album_active_no  = 0;
 	var last_chapter     = "";
-        mbox_mode            = 'Album'; 
+	mbox_mode            = 'Album'; 
 
-	// fill global cache
+	// fill global cache for reload on window resize
 	mbox_list_data = data;
 
 	// reset cover list (to show cover of all albums, playlists, ...)
@@ -117,8 +126,27 @@ function mboxAlbumAll(data) {
 
 	setTextById("frame2",filter);
 
-	// ..................................
+	// set data to place detailviews etc.
+	var album_in_row     = 0;
+	var album_sub_rows   = 0;
+	var albums_per_row   = mboxListCount_New();
+	var album_next_empty = false;
+	var row_per_chapter  = false;	
+	if (albums_per_row >= 9) {
+		row_per_chapter = true;
+		}
+	var chapter_detail  = 0;
+	var chapter_number  = 0;
+	var new_row         = false;
+	var new_sub_row     = false;
+	
+	// mboxAlbumAll_chapter() -> Chapter, starting with first character of e.g. artist
+	// mboxAlbumAll_empty()   -> Empty box, if row per chapter
+	// mboxAlbumAll_detail()  -> Album detail, to be filled when clicked on album (position dependend on rows)
+	// mboxCoverAlbum_new()   -> Album cover
+	// mboxAlbumAll_album()   -> Embedded album cover with tool tips etc.
 
+	// Sort entries (by artist)
 	var sorted_entries = [];
 	for (var key in album_info) {
 		var sort_string = "";
@@ -129,84 +157,114 @@ function mboxAlbumAll(data) {
 		sorted_entries.push( sort_string );
 		}
 	sorted_entries.sort();
-
+	
 	// list albums
 	var i              = 1;
-	var chapter_count  = 0;
-	var chapter_detail = 0;
+	var album_per_row  = 0;
 	if (sorted_entries.length == 0) { text += "<div>" + lang("NODATA_RELOAD") + "</div>"; }
-		
 	for (var a=0;a<sorted_entries.length;a++) {
 
+		var cover   = default_cover;
 		var keys    = sorted_entries[a].split("||");
 		var uuid    = keys[3];
 		var album   = keys[2];
 		var artist  = keys[1];
-		var chapter = keys[0];
+		var chapter = keys[0].substring(0,1);
 		var text1   = "";
 		var print1  = "";
 		
-		var isvalidfilter	= false;
-		var mbox_list_count	= mboxListCount();
+		var album_line          = false;
+		var album_detail        = false;
+		var album_empty         = false;
+		var album_character	 = false;
+		var album_detail_last   = false;
+		var album_detail_next   = false;
+		var album_detail_number = 0;
+		
+		var isvalidfilter	 = false;
+		var mbox_list_count	 = mboxListCount();  /// REMOVE LATER
 
+		if (a == sorted_entries.length-1) { album_detail_last = true; }
 		if (the_filter[0] in album_info[uuid] && album_info[uuid][the_filter[0]].indexOf(the_filter[1]) > -1) { isvalidfilter = true; }
-
 		if (filters == "" || isvalidfilter) {
+		
+// letzter Eintrag mit row_per_chapter == true
+// mehrzeilige Einträge mit row_per_chapter == true
+		
+			// check if new row without chapters
+			if (mbox_show_char == false) {
+				if (Math.round(a+1/albums_per_row) == (a+1/albums_per_row))	{ new_row = true;  }
+				else								{ new_row = false; }			
+				}
 
 			// print cover with charakter and empty album, if new line per chapter
-			if (mbox_show_char) {
-							
+			else {
 				if (last_chapter != chapter) {
-					text1 = mboxAlbumAll_section(i,chapter,last_chapter);
-					if (text1 != "") { 				
-						i++; 
-						if (chapter_detail > 0)		{text += mboxAlbumAll_detail(chapter_detail,last_chapter);}
-						if (mbox_list_count > 6 && a > 1)	{text += "<hr style='float:left;width:99%;border:#aaa solid 0.5px;'/>";}
-						text          += text1;
-						chapter_count  = 1;
-						chapter_detail = 1;
+					i++;
+					chapter_number++;	
+					album_character = true;
+					if (row_per_chapter)		{
+						album_in_row    = 1;	
+						album_sub_rows  = 1;
+						new_row         = true; 
+						new_sub_row     = false;	
+						}
+					if (row_per_chapter && a > 1)	{ 
+						album_line      = true; 
+						album_detail    = true; 
 						}
 					}
 				else {
-					chapter_count++;
-					if (chapter_count == mbox_list_count-2)	{
-						chapter_detail++;
-						chapter_count       = 1;
-						text               += mboxAlbumAll_detail(chapter_detail-1,chapter);
-						text               += mboxAlbumAll_empty(chapter_detail,chapter);
-						}
-					}					
-				last_chapter = chapter;
-				}
+					album_in_row++;
+					new_row      = false;
+					new_sub_row  = false;
+					if (album_next_empty)						{ album_next_empty = false; album_empty = true; album_detail = true; chapter_number++; }
+					if (row_per_chapter && album_in_row+1 == albums_per_row)	{ album_in_row = 1; album_next_empty = true; }
+					}
 
-			//console.log("SORT: "+sorted_artists[key].substring(0,1)+"_"+sorted_artists[key]+"/"+sorted_albums[key2]);
-			var onclick_open  = "";
-			if (mbox_show_char) {
-				var detail    = chapter.substring(0,1) + "_" + chapter_detail;
-				onclick_open  = "mboxAlbumList_load_direct('" + detail + "','" + i + "','" + uuid + "');"; 				// load album details
+				if (row_per_chapter == false) {
+					album_detail_number = (Math.trunc((i-1)/albums_per_row)+1) * albums_per_row;
+					console.log(album_detail_number);
+					}
 				}
-			else {	onclick_open  = "mboxAlbumList_load('" + i + "','" + uuid + "');"; 				// load album details
+			
+			if (a == sorted_entries.length) { album_detail_number = i; }
+			if (album_detail)		{ text += mboxAlbumAll_detail( chapter_number-1, last_chapter ); }
+			if (album_empty)	 	{ text += mboxAlbumAll_empty(  chapter_number, chapter ); }
+			if (album_line)		{ text += "<hr style='float:left;width:99%;border:#aaa solid 0.5px;'/>"; }
+			if (album_character)		{ text += mboxAlbumAll_chapter( chapter_number, chapter, last_chapter ); text += mboxHtmlEntryDetail( i-1 ); }
+			
+			//.............
+			
+			var onclick_play  = "appFW.requestAPI('GET',['play', '" + uuid + "'],'',mboxControl);"; 	// play album remote
+			var onclick_open  = "mboxAlbumList_load('" + album_detail_number + "','" + uuid + "');"; 	// load album details
+			
+			if (row_per_chapter) {
+				chapter_number_x = chapter_number;
+				onclick_open  = "mboxAlbumList_load_direct('" + chapter + "_" + chapter_number_x + "','" + i + "','" + uuid + "');"; 				// load album details
 				}
 				
-			var onclick_play  = "appFW.requestAPI('GET',['play', '" + uuid + "'],'',mboxControl);"; 	// play album remote
-			var cover    	  = default_cover;
-
 			if (uuid == album_active) { album_active_no = i; }
 			if (uuid) {
-				cover           = mboxCoverAlbum_new(uuid,album_info);         // Check if Cover exists
-				[text1, print1] = mboxAlbumAll_album(i,uuid,album,artist,cover,onclick_open,onclick_play);
-				if (text1 != "") { i++; text += text1; print += print1; }   // text = album list; print is cover for print out
+				cover           = mboxCoverAlbum_new( uuid, album_info );  // Check if Cover exists
+				[text1, print1] = mboxAlbumAll_album( i, uuid, album, artist, cover, onclick_open, onclick_play );
+				if (text1 != "") { i++; text += text1; print += print1; }  // text = album list; print is cover for print out
 				}
+
+			//.............
+			
+			if (album_detail_last)	{ text += mboxAlbumAll_detail( chapter_number, chapter ); }
+
+			last_chapter = chapter;
 			}
-			//if (Number.isInteger(a / 6)) { setTextById("frame2",text); }
+
 		mbox_list_amount = i;
 		}
 	text  += mboxAlbumAll_detail(chapter_detail,last_chapter);
-	
 	print += mboxCoverListEnd();
 
-	setTextById("frame3",text);
-	setTextById("ontop",print);
+	setTextById("frame3", text);
+	setTextById("ontop", print);
 
 	mboxControlToggleFilter_show();
 
@@ -220,7 +278,7 @@ function mboxAlbumAll(data) {
 // chapter list entry
 //---------------------------------------------------------
 
-function mboxAlbumAll_section(count,title,last_title) {
+function mboxAlbumAll_chapter(count, title, last_title) {
 
 	var text     = "";
 	var cover    = mbox_icon_dir + mbox_icons["album"];
@@ -228,12 +286,10 @@ function mboxAlbumAll_section(count,title,last_title) {
 	last_title += " ";
 	last_char   = last_title.substring(0,1);
 
-	if (act_char && act_char != last_char) {
-		text += "<div class=\"album_cover character\" style=\"background:url(" + cover + ");\">";
-		text += "<div class=\"album_sort\">" + act_char + "</div>";
-		text += "</div>";
-		text += "<div class=\"album_detail\" id=\"album_" + count + "\" style=\"display:none\">test " + count + " / " + document.body.clientWidth + "</div>";
-		}
+	text += "<div class=\"album_cover character\" style=\"background:url(" + cover + ");\">";
+	text += "<div class=\"album_sort\">" + act_char + "</div>";
+	text += "</div>";
+	text += "<div class=\"album_detail\" id=\"album_" + count + "\" style=\"display:none\">test " + count + " / " + document.body.clientWidth + "</div>";
 
 	return text;
 	}

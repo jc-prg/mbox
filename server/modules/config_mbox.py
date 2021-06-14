fe8a5d1a91c5829c3fb91356ecd370036b9adb7f
Christoph <email@ckloth.de>
2021-06-14 23:17:00 +0200

    app/srv: update version info

--- server/modules/config_mbox.py
+++ server/modules/config_mbox.py
@@ -13,10 +13,10 @@ APIname       = "jc://mbox/server/"
 APIname_LED   = "jc://mbox/LED/"
 APIname_BTN   = "jc://mbox/BUTTON/"
 APIname_RFID  = "jc://mbox/RFID/"
-APIversion    = "v0.7.5"
-APPversion    = "v1.1.1"
+APIversion    = "v0.8.0"
+APPversion    = "v1.1.2"
 APPsupport    = [APPversion, 
-                "v1.1.0", "v1.0.0",
+                "v1.1.1", "v1.1.0", "v1.0.0",
                 "v0.7.1", "v0.7.0",
                 ]  # other supported versions
 

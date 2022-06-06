cef6aca331cd9a86c581cb868dce76a5e5bfbe26
Christoph <email@ckloth.de>
2022-06-06 20:53:38 +0200

    srv: show error if button on the box is pressed > 10s

--- server/server_buttons.py
+++ server/server_buttons.py
@@ -97,7 +97,7 @@ def loop():
                     else:
                         same_key += 1
 
-            if GPIO.input(pins[last_key]) != GPIO.LOW:
+            if last_key != "" and GPIO.input(pins[last_key]) != GPIO.LOW:
                 same_key = 0
                 last_key = ""
 

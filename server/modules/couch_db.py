f2dc179e22145c8ec4fd8963237698c314d72f85
Christoph <email@ckloth.de>
2022-06-07 12:42:57 +0200

    srv: debugging loading radio/podcast

--- server/modules/couch_db.py
+++ server/modules/couch_db.py
@@ -173,10 +173,12 @@ class CouchDB:
         """
         if entry_key == "" and db_key in self.cache:
             self.logging.debug("CouchDB read cache: " + db_key + " " + str(time.time()))
-            return self.cache[db_key].copy()
+            return self.cache[db_key]
+
         elif db_key in self.cache and entry_key in self.cache[db_key]:
             self.logging.debug("CouchDB read cache: " + db_key + "/" + entry_key + "/" + (time.time()))
-            return self.cache[db_key][entry_key].copy()
+            return self.cache[db_key][entry_key]
+
         else:
             self.logging.warning("CouchDB read cache: " + db_key + " doesn't exist in cache")
             return

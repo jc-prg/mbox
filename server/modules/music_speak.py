adb0ae9188d83756dea3ad13fcd5043ce877bb4e
Christoph <email@ckloth.de>
2022-06-14 09:19:48 +0200

    srv: bugfix and unit test for music_speak.py

--- server/modules/music_speak.py
+++ server/modules/music_speak.py
@@ -18,6 +18,8 @@ class SpeakThread(threading.Thread):
         self.stopProcess = False
         self.volume = 70
         self.running = True
+        self.language = stage.language
+        self.speak_msg = stage.speak_msg
 
         self.start_time = start_time
         self.vlc_player = vlc_player
@@ -40,45 +42,51 @@ class SpeakThread(threading.Thread):
         """
         self.running = False
 
-    def speak_text(self, text, volume=None):
+    def speak_text(self, text, volume=None, filename=None, create_only=False):
         """
         Use Google API to speech from text
         """
         self.logging.debug("speak_text // " + text)
-        filename = "/tmp/music-box-speech.mp3"
-        language = stage.language.lower()
+        if filename is None:
+            filename = "/tmp/music-box-speech.mp3"
+        language = self.language.lower()
         duration = 0
 
         try:
             tts = gtts.gTTS(text=text, lang=language)
             tts.save(filename)
         except Exception as e:
-            self.logging.error("Could not speak message (" + text + ").")
+            self.logging.error("Could not create message to speak (" + text + "::" + filename + ").")
             self.logging.error(" -> gtts error: " + str(e))
+            return "error"
 
-        try:
-            current_volume = self.vlc_player.volume
-            self.vlc_player.mute(False)
-            if volume is None:
-                self.vlc_player.set_volume(self.volume)
-            else:
-                self.vlc_player.set_volume(volume)
-            self.vlc_player.play(filename, wait=False)
-            duration = self.vlc_player.media.get_duration() / 1000
-            time.sleep(duration)
-            self.vlc_player.set_volume(current_volume)
-        except Exception as e:
-            self.logging.error("Could not speak message (" + text + ").")
-            self.logging.error(" -> player error: " + str(e))
+        if not create_only:
+            try:
+                current_volume = self.vlc_player.volume
+                self.vlc_player.mute(False)
+                if volume is None:
+                    self.vlc_player.set_volume(self.volume)
+                else:
+                    self.vlc_player.set_volume(volume)
+                self.vlc_player.play(filename, wait=False)
+                duration = self.vlc_player.media.get_duration() / 1000
+                time.sleep(duration)
+                self.vlc_player.set_volume(current_volume)
+            except Exception as e:
+                self.logging.error("Could not speak message (" + text + ").")
+                self.logging.error(" -> player error: " + str(e))
+                return "error"
 
-        self.logging.info(" - Speak_text: " + str(text) + " (" + str(duration) + ")")
+            self.logging.info(" - Speak_text: " + str(text) + " (" + str(duration) + ")")
+
+        return "ok"
 
     def speak_message(self, message):
         """
         play spoken messages from prerecorded files
         """
-        if stage.speak_msg != "yes":
-            return
+        if self.speak_msg.lower() != "yes":
+            return "off"
 
         self.logging.debug("speak_message // " + message)
         current_volume = self.vlc_player.volume
@@ -86,20 +94,29 @@ class SpeakThread(threading.Thread):
         self.vlc_player.set_volume(self.volume)
 
         directory_path = os.getcwd()
-        filename = os.path.join(directory_path, mbox.error_msg_dir + stage.language + "_" + message + ".mp3")
+        filename = os.path.join(directory_path, mbox.error_msg_dir + self.language + "_" + message + ".mp3")
         filename_EN = os.path.join(directory_path, mbox.error_msg_dir + "EN_" + message + ".mp3")
-        filename_UE = os.path.join(directory_path, mbox.error_msg_dir + stage.language + "_UNKNOWN-ERROR.mp3")
+        filename_UE = os.path.join(directory_path, mbox.error_msg_dir + self.language + "_UNKNOWN-ERROR.mp3")
         filename_UE_EN = os.path.join(directory_path, mbox.error_msg_dir + "EN_UNKNOWN-ERROR.mp3")
 
         if os.path.isfile(filename):
             self.vlc_player.play(filename, True)
+            self.vlc_player.set_volume(current_volume)
         elif os.path.isfile(filename_EN):
             self.vlc_player.play(filename_EN, True)
+            self.vlc_player.set_volume(current_volume)
         elif os.path.isfile(filename_UE):
             self.vlc_player.play(filename_UE, True)
-        else:
+            self.vlc_player.set_volume(current_volume)
+            return self.language.upper()+"_UNKNOWN-ERROR"
+        elif filename_UE_EN:
             self.vlc_player.play(filename_UE_EN, True)
+            self.vlc_player.set_volume(current_volume)
+            return "EN_UNKNOWN-ERROR"
+        else:
+            self.vlc_player.set_volume(current_volume)
+            return "error"
 
         # duration = self.vlc_player.player.get_length() / 1000
         # time.sleep(duration - 0.3)
-        self.vlc_player.set_volume(current_volume)
+        return self.language.upper()+"_"+message
--- server/modules/test_music_speak.py
+++ server/modules/test_music_speak.py
@@ -1,15 +1,58 @@
 from unittest import TestCase
+import time
+import modules.music_vlc as music_vlc
+import modules.music_speak as music_speak
 
 
 class TestSpeakThread(TestCase):
+
+    def vlc_start(self):
+        self.vlc = music_vlc.VlcThread("test")
+        self.vlc.start()
+
+    def vlc_end(self):
+        self.vlc.stop()
+
     def test_run(self):
-        self.fail()
+        self.vlc_start()
+        self.speak = music_speak.SpeakThread(self.vlc, "test")
+        self.speak.start()
+        time.sleep(1)
+        self.assertEqual(self.speak.running, True)
+        self.speak.stop()
+        self.vlc_end()
 
     def test_stop(self):
-        self.fail()
+        self.vlc_start()
+        self.speak = music_speak.SpeakThread(self.vlc, "test")
+        self.speak.start()
+        time.sleep(1)
+        self.assertEqual(self.speak.running, True)
+        self.speak.stop()
+        time.sleep(1)
+        self.assertEqual(self.speak.running, False)
+        self.vlc_end()
 
     def test_speak_text(self):
-        self.fail()
+        self.vlc_start()
+        self.speak = music_speak.SpeakThread(self.vlc, "test")
+        self.speak.start()
+        self.speak.language = "EN"
+        self.assertEqual(self.speak.speak_text("This is a text to speech test."), "ok")
+        self.vlc_end()
 
     def test_speak_message(self):
-        self.fail()
+        self.vlc_start()
+        self.vlc.set_volume(50)
+        self.speak = music_speak.SpeakThread(self.vlc, "test")
+        self.speak.start()
+        self.speak.speak_msg = "no"
+        self.assertEqual(self.speak.speak_message("STARTING"), "off")
+        self.speak.speak_msg = "yes"
+        self.speak.language = "EN"
+        self.assertEqual(self.speak.speak_message("STARTING"), "EN_STARTING")
+        self.speak.language = "DE"
+        self.assertEqual(self.speak.speak_message("STARTING"), "DE_STARTING")
+        self.assertEqual(self.speak.speak_message("XYXYXYXYX"), "DE_UNKNOWN-ERROR")
+        self.assertEqual(self.vlc.volume, 50)
+        self.vlc_end()

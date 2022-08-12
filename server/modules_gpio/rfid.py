c432c4f6fbef24ab7f38e4254c9743b8c4de819b
Christoph <email@ckloth.de>
2022-08-12 13:38:36 +0200

    srv: rfid info if still alive

--- server/modules_gpio/rfid.py
+++ server/modules_gpio/rfid.py
@@ -2,88 +2,97 @@
 # -*- coding: utf8 -*-
 
 import RPi.GPIO as GPIO
-import modules_rfid.MFRC522      as MFRC522
-import modules.jcJson            as jcJSON
-import modules.config_stage      as stage
+import modules_rfid.MFRC522 as MFRC522
+import modules.jcJson as jcJSON
+import modules.config_stage as stage
 import signal
 import threading
 import time
 import logging
 
-#-----------------------------------
+# -----------------------------------
 # init
-#-----------------------------------
+# -----------------------------------
 
-exitFlag  = 0
+exitFlag = 0
 rfid_ctrl = {}
 
 logging.basicConfig(level=logging.INFO)
 
+
 # read from file, which stage should be use ... to switch between stages during runtime
 def get_active_stage():
-  settings = jcJSON.read("../../active")
-  return settings["active_stage"]
+    settings = jcJSON.read("../../active")
+    return settings["active_stage"]
 
-if stage.test == True:  this_stage = "test"
-else:                   this_stage = "prod"
 
-#------------------------------
+if stage.test:
+    this_stage = "test"
+else:
+    this_stage = "prod"
+
+
+# ------------------------------
 
 # THREADING CLASS
 
-class rfidThread (threading.Thread):
+class rfidThread(threading.Thread):
 
-   def __init__(self, threadID, name, counter):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name     = name
-      self.counter  = counter
-      self.stopProcess = False
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.stopProcess = False
 
-   def run(self):
-      logging.info( "Starting " + self.name )
-      rfidRead(self)
-      logging.info( "Exiting " + self.name )
+    def run(self):
+        logging.info("Starting " + self.name)
+        rfidRead(self)
+        logging.info("Exiting " + self.name)
 
-   def stop(self):
-      self.stopProcess = True
+    def stop(self):
+        self.stopProcess = True
 
 
 # Create an object of the class MFRC522
 MIFAREReader = MFRC522.MFRC522()
 
+
 # Check card reader for ID
 def rfidRead(thread):
+    global MIFAREReader, rfid_ctrl, continueReading
 
-  global MIFAREReader, rfid_ctrl, continueReading
+    # endless loop
+    continueReading = True
+    count = 0
+    while continueReading and not thread.stopProcess:
 
-  # endless loop
-  continueReading = True
-  while continueReading and not thread.stopProcess:
+        # Wait a few seconds
+        time.sleep(0.5)
+        count += 1
 
-    # Wait a few seconds
-    time.sleep(0.5)
+        if count/5 == int(count/5):
+            logging.info("RFID active")
 
-    # check if ative stage
-    if (get_active_stage() == this_stage):
+        # check if ative stage
+        if (get_active_stage() == this_stage):
 
-      # Scan for card
-      (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+            # Scan for card
+            (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-      # If a card is found
-      if status == MIFAREReader.MI_OK:
-        logging.info( "Card detected" )
+            # If a card is found
+            if status == MIFAREReader.MI_OK:
+                logging.info("Card detected")
 
-      # Get the UID of the card
-      (status,uid) = MIFAREReader.MFRC522_Anticoll()
+            # Get the UID of the card
+            (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
-      # If we have the UID, continue
-      if status == MIFAREReader.MI_OK:
+            # If we have the UID, continue
+            if status == MIFAREReader.MI_OK:
+                # Print UID
+                logging.info("Card read UID: " + str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3]))
+                rfid_ctrl["cardUID"] = str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3])
 
-        # Print UID
-        logging.info("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
-        rfid_ctrl["cardUID"] = str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
+                time.sleep(5)
 
-        time.sleep(5)
-
-        rfid_ctrl["cardUID"] = ""
+                rfid_ctrl["cardUID"] = ""
